f72909b57e543d430338e7890a21b1a8af7d6cf1
vishal <vishalbiradar205@gmail.com>
2017-11-11 21:49:51 +0530

    commited db

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
 
 config.serve_static_assets = true
 
-config.public_file_server.enabled = true
+
 
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
