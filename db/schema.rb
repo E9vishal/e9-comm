e63b4e6bdb7bccc26db2116d255bbef88a8a8055
vishal <vishalbiradar205@gmail.com>
2017-11-15 15:35:43 +0530

    added sample data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031101443) do
+ActiveRecord::Schema.define(version: 20171114135718) do
 
   create_table "connections", force: :cascade do |t|
     t.integer  "Sequence_Number"
