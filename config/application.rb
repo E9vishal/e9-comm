f51864973dd1de7b060f1978c63a889852a61ddc
vishal <vishalbiradar205@gmail.com>
2017-12-06 16:58:08 +0530

    added stylesheet

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Mystore
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.initialize_on_precompile = false
+    config.assets.initialize_on_precompile = true
 
     config.assets.paths << Rails.root.join("app", "assets", "stylesheets")
   end
